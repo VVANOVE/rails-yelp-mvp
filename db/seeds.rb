6778b003a71361b0afce2c842a430afe707ff1f3
victorvanoverschelde <victorvanoverschelde@gmail.com>
2021-06-09 15:59:17 +0200

    restaurants MCV ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'La Gondole', address: '144 avenue de dunkerque', category: 'italian')
+Restaurant.create(name: 'Le Palais des Delices', address: '50 rue solferino', category: 'chinese')
+Restaurant.create(name: 'Le Vieux qui pu', address: '2 rue des commodités', category: 'french')
+Restaurant.create(name: 'La grande frite', address: '58 rue Leon', category: 'Belgian')
+Restaurant.create(name: 'HijoHito', address: '8 rue guy moquet', category: 'Japonese')
